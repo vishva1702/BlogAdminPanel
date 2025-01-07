8f9afa34ee50e681e1f9a60d03ef94ee17c5e5a9
VISHVA <VISHVA@LAPTOP-7HLIR41R>
2025-01-07 10:36:41 +0530

    Commit

--- BlogAdminPanel/Data/ApplicationDbContext.cs
+++ BlogAdminPanel/Data/ApplicationDbContext.cs
@@ -8,10 +8,6 @@ namespace BlogAdminPanel.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<BlogPost> BlogPosts { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<User> Users { get; set; }
+       
     }
 }
