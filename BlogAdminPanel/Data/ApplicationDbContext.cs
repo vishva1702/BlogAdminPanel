8910f2155763713e552424d2f2f0ec54faf93f04
VISHVA <VISHVA@LAPTOP-7HLIR41R>
2025-01-07 10:32:16 +0530

    Commited

--- BlogAdminPanel/Data/ApplicationDbContext.cs
+++ BlogAdminPanel/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BlogAdminPanel.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlogAdminPanel.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ BlogAdminPanel/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogAdminPanel.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
