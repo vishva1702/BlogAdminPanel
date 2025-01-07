14bd4cf4edcea24b2d7f3028b808d1027565a87c
VISHVA <VISHVA@LAPTOP-7HLIR41R>
2025-01-07 10:40:21 +0530

    Commit

--- BlogAdminPanel/Data/Migrations/20250107050911_Init.Designer.cs
+++ BlogAdminPanel/Data/Migrations/20250107050911_Init.Designer.cs
@@ -4,6 +4,7 @@ using BlogAdminPanel.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogAdminPanel.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250107050911_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
